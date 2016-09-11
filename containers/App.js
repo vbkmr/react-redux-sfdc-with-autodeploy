db25f1a980efd1041b6eb3f6ee91fab11a9fba85
Vaibhav.Kumar <vaibhav.iiitj@gmail.com>
2016-09-11 18:46:52 +0900

    wercker added

--- containers/App.js
+++ containers/App.js
@@ -11,20 +11,52 @@ how the stores' state and dispatch are mapped to the props fo the component
 class App extends Component {
   constructor(props) {
     super(props)
+    this.handleRemoteLoginAction = this.handleRemoteLoginAction.bind(this)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
   }
 
   componentDidMount() {
+    this.setState({tableData: '', tableMessage: '一覧部 '})
   }
 
   componentWillReceiveProps(nextProps) {
   }
 
+  handleFormSubmit(){
+    var queryParams = {}
+    MyReactAppApexController.getSearchedAccounts(JSON.stringify(queryParams), this.handleRemoteLoginAction)
+  }
+
+  //HANDLES remote-action return value from Force.com
+  handleRemoteLoginAction(result,event){
+    //console.log(`result: ${JSON.stringify(result)}, event: ${JSON.stringify(event)}`)
+    if(event.type == 'exception') {
+         alert(event.message);
+     } else {
+        //console.log(`result: ${JSON.stringify(result)}`)
+        this.setState({tableData: result, tableMessage: '一覧部 '})
+     }
+     if(result == null || result.length == 0){
+        this.setState({tableData: '',tableMessage: '一覧部 '})
+     }
+  }
+
   render() {
+    let tableData
+    if(this.state.tableData === '')
+       tableData = 'No records to display.'
+    else tableData = this.state.tableData
     return (
-      <div className={classNames(styles['panel'],styles['panel-default'])}>
-        <p>
-        {'This is my App!'}
-        </p>
+      <div>
+        <form className={styles['form-horizontal']} onSubmit={this.handleFormSubmit}>
+          <div className={classNames(styles['panel'],styles['panel-default'])}>
+            <p>Fetch my Salesforce data app</p>
+            <div className={classNames(styles['col-sm-offset-2'],styles['col-sm-8'])}>
+              <button  type="submit" className={classNames(styles['btn'],styles['btn-block'],styles['btn-primary'])}>Fetch SOQL data</button>
+            </div>
+            {tableData}
+          </div>
+        </form>
       </div>
     )
   }
