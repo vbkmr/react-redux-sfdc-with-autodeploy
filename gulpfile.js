c19a17269fc41108c05721eeab38dbc4b85c9487
Vaibhav.Kumar <vaibhav.iiitj@gmail.com>
2016-09-12 00:14:11 +0900

    gulp updated

--- gulpfile.js
+++ gulpfile.js
@@ -32,14 +32,14 @@ gulp.task( 'deploy', ['resource-build'], function() {
            password: process.env.SF_PASSWORD, //Should be set as wercker pipleine env. var
            loginUrl: process.env.SF_ENV_URL //Should be set as wercker pipleine env. var
        } ) )
-      .on('error', gutil.log);
+      .on('error', handleError);
 } );
 
 //Static resource build of webpack dist.
 gulp.task( 'resource-build', ['webpack'], function() {
    return gulp.src( './dist/*' ).pipe( zip( 'ReactSearchStatic.resource' ) )
               .pipe( gulp.dest( 'pkg/staticresources' ) )
-              .on('error', gutil.log);
+              .on('error', handleError);
 } );
 
 //Webpack build of react-redux
@@ -47,5 +47,5 @@ gulp.task( 'webpack', function() {
    return gulp.src( 'index.js' )
    .pipe(webpack( require('./webpack.config.js') ))
    .pipe( gulp.dest( './dist' ) )
-   .on('error', gutil.log);
+   .on('error', handleError);
 } );
